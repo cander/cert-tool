bed6d65b841ae87a142434aad315c47a9375a344
Charles Anderson <master.sparkle@gmail.com>
2018-03-30 16:12:17 -0700

    Restructure code
    Starting to flesh out some more classes/modules.

--- cert_bundle.rb
+++ cert_bundle.rb
@@ -1,31 +1,49 @@
 class Certificate
-    def initialize(x509_text)
+    def initialize(x509_text, pem_text)
         @x509_text = x509_text
+        @pem_text = pem_text
     end
 end
 
+module OpenSSL
+  # parse PEM text into the x509 dump from OpenSSL
+  def self.parse_cert(pem_text)
+    # throwing away error messages from openssl
+    result = `echo '#{pem_text}' | openssl x509 -noout -text 2> /dev/null`
+    unless $?.success?
+      raise ArgumentError.new("Failed to parse delimited PEM certificate")
+    end
+
+    result
+  end
+
+end
+
 class CertBundle
+    def initialize(certs)
+      @certficates = certs
+    end
+
+    def size
+      @certficates.size
+    end
+
     def self.parse_bundle_file(in_file)
-      result = []
-      cert_strs = ""
+      certs = []
+      pem_text = ""
       in_file.each do |line|
-        cert_strs += line
+        pem_text += line
         if (line =~ /^\-+END(\s\w+)?\sCERTIFICATE\-+$/)
-            # throwing away error messages from openssl
-            parsed_cert = `echo '#{cert_strs}' | openssl x509 -noout -text 2> /dev/null`
-            if $?.success?
-              result << Certificate.new(parsed_cert)
-              cert_strs = ""
-            else
-              raise ArgumentError.new("Failed to parse delimited certificate")
-            end
+            parsed_cert = OpenSSL.parse_cert(pem_text)
+            certs << Certificate.new(parsed_cert, pem_text)
+            pem_text = ""
         end
       end
 
-      if !cert_strs.empty?
+      if !pem_text.empty?
         raise ArgumentError.new("Failed to find end of certificate")
       end
 
-      result
+      CertBundle.new(certs)
     end
 end
