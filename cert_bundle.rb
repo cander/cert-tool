0bf9b5820246acd69b513c017c743b00e2065fa5
Charles Anderson <master.sparkle@gmail.com>
2018-03-30 17:30:44 -0700

    Begin verification process
    Between limitations of OpenSSL on MacOS and /bin/sh syntax (used
    by system(3)), this is more work than it should be.

--- cert_bundle.rb
+++ cert_bundle.rb
@@ -1,7 +1,19 @@
+require 'tempfile'
+
 class Certificate
-    def initialize(x509_text, pem_text)
+    attr_reader :pem_text
+
+    def initialize(x509_text, pem_text, index = 0)
         @x509_text = x509_text
         @pem_text = pem_text
+        @pem_file = Tempfile.new("cert-#{index}.pem")
+        puts @pem_file.path
+        @pem_file.write(pem_text)
+        @pem_file.close
+    end
+
+    def pem_path
+        @pem_file.path
     end
 end
 
@@ -17,6 +29,20 @@ module OpenSSL
     result
   end
 
+  def self.verify_chain(certs)
+    # assume MacOS where the command hast to look like:
+    # openssl verify -CAfile root.pem -untrusted <(cat intermediate.pems ) leaf.pem 
+    # because it won't take multiple -untrusted flags
+    # and, assume we have at least 3 certs
+    num_certs = certs.size
+    root_pem = certs[-1].pem_path
+    leaf_pem = certs[0].pem_path
+    puts `openssl verify #{leaf_pem}`
+    #puts `openssl verify -CAfile <(echo #{root_pem}) <(echo #{leaf.pem })`
+
+  end
+
+
 end
 
 class CertBundle
@@ -28,15 +54,26 @@ class CertBundle
       @certficates.size
     end
 
+    def pem_text(idx)
+      @certficates[idx].pem_text
+    end
+
+    def verify
+      puts "Verify chain of #{@certficates.size} certs..."
+      OpenSSL.verify_chain(@certficates)
+    end
+
     def self.parse_bundle_file(in_file)
       certs = []
+      cert_num = 1
       pem_text = ""
       in_file.each do |line|
         pem_text += line
         if (line =~ /^\-+END(\s\w+)?\sCERTIFICATE\-+$/)
             parsed_cert = OpenSSL.parse_cert(pem_text)
-            certs << Certificate.new(parsed_cert, pem_text)
+            certs << Certificate.new(parsed_cert, pem_text, cert_num)
             pem_text = ""
+            cert_num += 1
         end
       end
 
@@ -46,4 +83,12 @@ class CertBundle
 
       CertBundle.new(certs)
     end
+end
+
+
+if __FILE__ == $0
+  certs = CertBundle.parse_bundle_file(ARGF)
+  puts "Found #{certs.size} certificates"
+  certs.verify
+
 end
